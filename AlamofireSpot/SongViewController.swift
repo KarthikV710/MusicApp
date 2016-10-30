d218f9f1a97dad3f9d7e2bb9d1bdd740314b1c55
Karthik Muppala <karthik_muppala@apple.com>
2016-10-30 01:41:59 -0700

    Loading song list using spotify API, Play/pause song

---
+++ AlamofireSpot/SongViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SongViewController.swift
+//  
+//
+//  Created by Karthik Muppala on 10/30/16.
+//
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class SongViewController: UIViewController {
+    
+    var image = UIImage()
+    var name = String()
+    var link = String()
+    
+    
+    
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var songName: UILabel!
+    @IBOutlet weak var songImage: UIImageView!
+    
+    override func viewDidLoad() {
+        songName.text = name
+        songImage.image = image
+        backgroundImageView.image = image
+        playPauseButton.setTitle("Pause", for: .normal)
+        downloadSongFromUrl(downloadUrl: URL(string: link)!)
+
+    }
+    
+    func downloadSongFromUrl(downloadUrl: URL){
+        var downloadTask = URLSessionDownloadTask()
+        downloadTask = URLSession.shared.downloadTask(with: downloadUrl, completionHandler: {
+            customUrl, response, error in
+            self.playSong(url: customUrl!)
+        })
+        downloadTask.resume()
+    }
+    
+    func playSong(url: URL) {
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            player.play()
+        }catch{
+            print(error)
+        }
+    }
+    
+    @IBAction func playPauseAction(_ sender: AnyObject) {
+        
+        if player.isPlaying {
+            player.pause()
+            playPauseButton.setTitle("Play", for: .normal)
+        }else{
+            player.play()
+            playPauseButton.setTitle("Pause", for: .normal)
+        }
+    }
+    
+    
+}
--- AlamofireSpot/ViewController.swift
+++ AlamofireSpot/ViewController.swift
@@ -7,13 +7,94 @@
 //
 
 import UIKit
+import Alamofire
+import AVFoundation
 
-class ViewController: UIViewController {
+var player = AVAudioPlayer()
 
+
+struct song {
+    let songImage : UIImage!
+    let songName : String!
+    let songUrl : String!
+    
+}
+
+class TableViewController: UITableViewController {
+    var searchUrl = "https://api.spotify.com/v1/search?q=shawn+mendes&type=track"
+    typealias jsonFetchString = [String:AnyObject]
+    var songs = [song]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchUrl)
     }
+    
+    func callAlamo(url:String) {
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(jsonData: response.data!)
+        })
+    }
+    
+    func parseData(jsonData : Data) {
+        do{
+            var readableJson = try JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) as! jsonFetchString
+            if let tracks = readableJson["tracks"] as? jsonFetchString  {
+                if let items = tracks["items"] as? [jsonFetchString]{
+                    for i in 0..<items.count{
+                        let item = items[i]
+                        print(item)
+                        let name = item["name"] as! String
+                        let preveiwUrl = item["preview_url"] as! String
+                        
+                        if let album = item["album"] as? jsonFetchString {
+                            if let images = album["images"] as? [jsonFetchString]{
+                                let imageUrlData = images[0]
+                                let imageUrl = URL(string: imageUrlData ["url"] as! String)
+                                let imageData = NSData(contentsOf: imageUrl!)
+                                let image = UIImage(data:imageData as! Data)
+                                songs.append(song.init(songImage: image, songName: name, songUrl: preveiwUrl))
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        let songImage = cell?.viewWithTag(2) as! UIImageView
+        songImage.image = songs[indexPath.row].songImage
+        let songLabel = cell?.viewWithTag(1) as! UILabel
+        songLabel.text = songs[indexPath.row].songName
+        return cell!
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath = self.tableView.indexPathForSelectedRow?.row
+        
+        let vc = segue.destination as! SongViewController
+        vc.image = songs[indexPath!].songImage
+        vc.name = songs[indexPath!].songName
+        vc.link = songs[indexPath!].songUrl
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
